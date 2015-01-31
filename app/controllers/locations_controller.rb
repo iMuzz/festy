a971ccc77e811b962140bc15bd8e18badffefdcf
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-31 13:05:23 -0800

    remove pry

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,6 @@
 class LocationsController < ApplicationController
 
   def index
-    binding.pry
     @upcoming_festivals = Location.find_by(city: params[:location][:city].downcase).upcoming_festivals
   end
 
