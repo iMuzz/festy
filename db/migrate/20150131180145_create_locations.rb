650920880a0521e4b7272bd193e0b4f6f80527eb
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-31 10:02:13 -0800

    rails g locations

---
+++ db/migrate/20150131180145_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      t.string :country
+      t.string :
+
+      t.timestamps null: false
+    end
+  end
+end
