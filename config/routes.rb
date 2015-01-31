2c3ac408968025ae15b30e7757c419312bc37074
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-31 14:37:01 -0800

    create route for upcoming festivals

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :upcoming_festivals
   resources :locations
   root to: "welcomes#home"
   # The priority is based upon order of creation: first created -> highest priority.
