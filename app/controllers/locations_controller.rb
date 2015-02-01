873f15311b56151b5fcf7aaa37759fa7b9ef8cea
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-31 19:42:11 -0800

    fix error on nil

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,11 @@
 class LocationsController < ApplicationController
 
   def index
-    @upcoming_festivals = Location.find_by(city: params[:location][:city].downcase).upcoming_festivals.limit(6)
+    if Location.find_by(city: params[:location][:city].downcase).nil?
+      redirect_to root_url
+    else
+      @upcoming_festivals = Location.find_by(city: params[:location][:city].downcase).upcoming_festivals.limit(6)
+    end
   end
 
 end
